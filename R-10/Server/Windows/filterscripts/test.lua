0433d597e361de6720c58ce1c6521321e79e3eda
Arkhan <49446701+DamianQualshy@users.noreply.github.com>
2024-06-29 21:55:50 +0200

    R-10 Windows Server

---
+++ R-10/Server/Windows/filterscripts/test.lua
@@ -0,0 +1,129 @@
+function OnFilterscriptInit()
+	print("-------------------------------------------");
+	print("Description filterscript");
+	print("-------------------------------------------");
+
+        Enable_OnPlayerKey(1);
+end
+
+function OnFilterscriptExit()
+	print("------------------------");
+	print("Removing filterscript...");
+	print("------------------------");
+end
+
+function OnPlayerChangeClass(playerid, classid)
+ 
+end
+
+function OnPlayerSelectClass(playerid, classid)
+ 
+end
+
+function OnPlayerConnect(playerid)
+ 
+end
+
+function OnPlayerDisconnect(playerid, reason)
+
+end
+
+function OnPlayerSpawn(playerid, classid)
+ 
+end
+
+function OnPlayerDeath(playerid, p_classid, killerid, k_classid)
+ 
+end
+
+function OnPlayerHit(playerid, killerid)
+ 
+end
+
+function OnPlayerUnconscious(playerid, p_classid, killerid, k_classid)
+
+end
+
+function OnPlayerText(playerid, text)
+
+end
+
+function OnPlayerCommandText(playerid, cmdtext)
+
+    if cmdtext == "/ice" then
+
+         SetPlayerInstance(playerid,"DRAGON_ICE");
+
+    elseif cmdtext == "/fire" then
+
+         SetPlayerInstance(playerid,"DRAGON_FIRE");
+
+    elseif cmdtext == "/hero" then
+
+         SetPlayerInstance(playerid,"PC_HERO");
+    
+    elseif cmdtext == "/get" then
+
+         local x,y,z = GetPlayerPos(playerid);
+         local angle = GetPlayerAngle(playerid);
+
+         local msg = string.format("%f %f %f %f",x,y,z,angle);
+         SendPlayerMessage(playerid,255,255,0,msg);
+
+    elseif cmdtext == "/khorinis" then
+
+         SetPlayerWorld(playerid,"NEWWORLD\\NEWWORLD.ZEN","START_OW_ORETRAIL");
+
+    elseif cmdtext == "/gd" then
+
+         SetPlayerWorld(playerid,"OLDWORLD\\OLDWORLD.ZEN","WP_INTRO13");
+
+    elseif cmdtext == "/tpgd" then
+         SetPlayerPos(playerid,1786.0162,247.9422,-163.0392);
+
+    elseif cmdtext == "/fat" then
+         SetPlayerFatness(playerid,5.0);
+
+    elseif cmdtext == "/scaler" then
+	SetPlayerScale(playerid,1.0,1.0,1.0);
+
+    elseif cmdtext == "/scalen" then
+	SetPlayerScale(playerid,1.1,1.1,1.1);
+
+    elseif cmdtext == "/armor" then
+	EquipArmor(playerid,"ITAR_KDF_L");
+
+    elseif cmdtext == "/des" then
+	SetServerDescription("Ouuu ju tacz maj tralalalalalalal");
+
+    elseif cmdtext == "/gam" then
+	SetGamemodeName("Panie, ta nazwa zostala zmieniona!");
+
+   elseif cmdtext == "/host" then
+	SetServerHostname("Zmienilim nazwe hosta, fuck yea!");
+    end
+end
+
+function OnPlayerChangeWorld(playerid, world)
+
+end
+
+function OnPlayerEnterWorld(playerid, world)
+
+end
+
+function OnPlayerDropItem(playerid, itemid, item_instance, amount, x, y, z)
+
+end
+
+function OnPlayerTakeItem(playerid, itemid, item_instance, amount, x, y, z)
+
+end
+
+function OnPlayerKey(playerid, kX, kY)
+
+    if kX == KEY_Y then
+       local x,y,z = GetPlayerPos(playerid);
+       SetPlayerPos(playerid,x, y + 300,z);
+    end
+end
